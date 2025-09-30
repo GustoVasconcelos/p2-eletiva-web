c9e9aaa3e0fc32869cb07470393020d9e0a33b12
GustoVasconcelos <augusto@lothuscorp.com.br>
2025-09-30 10:49:29 -0300

    Arrumado a view (index.php)

--- app/views/tablets/index.php
+++ app/views/tablets/index.php
@@ -9,24 +9,23 @@
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th>Código</th>
                     <th>Descrição</th>
                     <th>Fabricante</th>
                     <th>Numero de Série</th>
                     <th>Acessórios</th>
+                    <th>Ações</th>
                 </tr>
             </thead>
             <tbody>
                 <?php while ($tablet = $tablets->fetch(PDO::FETCH_ASSOC)): ?>
                     <tr>
-                        <td><?php echo htmlspecialchars($tablet['tabCod']); ?></td>
-                        <td><?php echo htmlspecialchars($tablet['tabDescricao']); ?></td>
-                        <td><?php echo htmlspecialchars($tablet['tabFabricante']); ?></td>
-                        <td><?php echo htmlspecialchars($tablet['tabNumeroSerie']); ?></td>
-                        <td><?php echo htmlspecialchars($tablet['tabAcessorios']); ?></td>
+                        <td><?php echo htmlspecialchars($tablet['tab_descricao']); ?></td>
+                        <td><?php echo htmlspecialchars($tablet['tab_fabricante']); ?></td>
+                        <td><?php echo htmlspecialchars($tablet['tab_numeroserie']); ?></td>
+                        <td><?php echo htmlspecialchars($tablet['tab_acessorios']); ?></td>
                         <td>
-                            <a href="index.php?action=edit&id=<?php echo $tablet['id']; ?>" class="btn btn-sm btn-info">Editar</a>
-                            <a href="index.php?action=delete&id=<?php echo $tablet['id']; ?>" class="btn btn-sm btn-danger" onclick="return confirm('Tem certeza?');">Excluir</a>
+                            <a href="index.php?action=edit&id=<?php echo $tablet['tab_cod']; ?>" class="btn btn-sm btn-info">Editar</a>
+                            <a href="index.php?action=delete&id=<?php echo $tablet['tab_cod']; ?>" class="btn btn-sm btn-danger" onclick="return confirm('Tem certeza?');">Excluir</a>
                         </td>
                     </tr>
                 <?php endwhile; ?>
